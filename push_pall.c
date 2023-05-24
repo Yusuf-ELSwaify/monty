5fbfaa2dea8e5c842bcad149b2f9a9209f96052d
Haidyasser <haidyasser3@gmaail.com>
2023-05-24 18:57:43 +0300

    push function

--- push_pall.c
+++ push_pall.c
@@ -1,6 +1,29 @@
 #include "monty.h"
 
 /**
+ * isdigit - checks if a string is a digit
+ * @arg: string to check
+ * Return: 1 if true, 0 if false
+ */
+
+int _isdigit(char *arg)
+{
+	int i = 0;
+
+	if (arg == NULL)
+		return (0);
+	if (arg[0] == '-')
+		i++;
+	while (arg[i])
+	{
+		if (arg[i] < '0' || arg[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
  * push - pushes an element to the stack
  * @stack: pointer to pointer to head of stack
  * @line_number: line number being executed from script file
@@ -11,7 +34,7 @@ void push(stack_t **stack, unsigned int line_number)
 	stack_t *new_node, *p = *stack;
 	char *arg = strtok(NULL, " \n\t");
 
-	if (arg == NULL || isdigit(*arg) == 0)
+	if (arg == NULL || _isdigit(arg) == 0)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
