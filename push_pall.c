3e548999131f8ad2d7964f5588d683ba72e6be1c
Haidyasser <haidyasser3@gmaail.com>
2023-05-24 18:43:49 +0300

    add nop function

--- push_pall.c
+++ push_pall.c
@@ -8,6 +8,34 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
+	stack_t *new_node, *p = *stack;
+	char *arg = strtok(NULL, " \n\t");
+
+	if (arg == NULL || isdigit(*arg) == 0)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = atoi(arg);
+	new_node->next = NULL;
+	if (p)
+	{
+		while (p->next)
+		p = p->next;
+		new_node->prev = p;
+		p->next = new_node;
+	}
+	else
+	{
+		new_node->prev = NULL;
+		*stack = new_node;
+	}
 }
 
 /**
