9c1a94e8b3b42576fc2d8c485d68b1bc03658248
Haidyasser <haidyasser3@gmaail.com>
2023-05-24 18:40:25 +0300

    add nop function

--- nop.c
+++ nop.c
@@ -2,5 +2,12 @@
 
 /**
  * nop - does nothing
- * 
+ * @stack: pointer to pointer to head of stack
+ * @line_number: line number being executed from script file
  */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+        (void)stack;
+        (void)line_number;
+}
